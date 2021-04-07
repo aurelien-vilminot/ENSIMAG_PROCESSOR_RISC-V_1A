8212e286a2695ef343d5804fa1b4c27d42998866
Pierre Arvy <arvyp@ensipc324.ensimag.fr>
2021-04-07 15:11:52 +0200

    Modif pour slt

--- program/autotest/slt.s
+++ program/autotest/slt.s
@@ -5,10 +5,15 @@
     lui x30, 0x00001
     slt x31, x29, x30
     slt x31, x30, x29
-    lui x30, 0
+    lui x30, 0x10000
+    lui x29, 0x12000
+    slt x31, x29, x30
+    slt x31, x30, x29
 
 	# max_cycle 100
 	# pout_start
     # 00000001
 	# 00000000
+    # 00000000
+    # 00000001
 	# pout_end
--- vhd/CPU_CND.vhd
+++ vhd/CPU_CND.vhd
@@ -20,35 +20,30 @@ architecture RTL of CPU_CND is
     signal ext : std_logic;
     signal z : std_logic;
     signal s : std_logic;
-    signal rs1_ext : signed(32 downto 0);
-    signal ALU_Y_ext : signed(32 downto 0);
-    signal res : signed(32 downto 0);
+    -- signal rs1_ext : signed(32 downto 0);
+    -- signal ALU_Y_ext : signed(32 downto 0);
+    signal res : unsigned(32 downto 0);
 
 
 begin
-    rs1_ext <= signed('1'&rs1) when ext='1' AND rs1(31)='1' else signed('0'&rs1);
-    ALU_Y_ext <= signed('1'&alu_y) when ext ='1' AND rs1(31)='1' else signed('0'&alu_y);
-	-- A relire ce qui est au dessus, s'assurer que c'est bon !!!!!!
+
+ext <= (not(IR(12) and not(IR(6)))) OR (IR(6) and not(IR(13)));
+res <= ('0' & rs1) - ('0' & alu_y) when ext='0' else (rs1(31) & rs1) - (alu_y(31) & alu_y);
+s <= res(32);
+z <= '1' when res=0 else '0';
+jcond <= ((IR(12) XOR z) AND NOT(IR(14))) OR ((IR(12) XOR s) AND IR(14));
+slt <= s;
 
 
-        -- if ext = '1' then
-        --     if rs1(31) = "1" then
-        --         rs1_ext <= "1"&rs1;
-        --     else
-        --         rs1_ext <= "0"&rs1;
-        --     end if;
-        --     if alu_y(31) = "1" then
-        --         ALU_Y_ext <= "1"&alu_y;
-        --     else
-        --         ALU_Y_ext <= "0"&alu_y;
-        --     end if;
-        -- end if;
-
-    res <= rs1_ext - ALU_Y_ext;
-    z <= '1' when res=0 else '0';
-    s <= res(32);
-    ext <= (NOT(IR(12)) AND NOT(IR(6))) OR (NOT(IR(13)) AND IR(6));
-    jcond <= ((IR(12) XOR z) AND NOT(IR(14))) OR ((IR(12) XOR s) AND IR(14));
-    slt <= s;
+    -- -- Notre partie
+    -- ext <= (NOT(IR(12)) AND NOT(IR(6))) OR (NOT(IR(13)) AND IR(6));
+    -- rs1_ext <= signed('1'&rs1) when ext='1' AND rs1(31)='1' else signed('0'&rs1);
+    -- ALU_Y_ext <= signed('1'&alu_y) when ext ='1' AND rs1(31)='1' else signed('0'&alu_y);
+	-- -- A relire ce qui est au dessus, s'assurer que c'est bon !!!!!!
+    -- res <= rs1_ext - ALU_Y_ext;
+    -- z <= '1' when res=0 else '0';
+    -- s <= res(32);
+    -- jcond <= ((IR(12) XOR z) AND NOT(IR(14))) OR ((IR(12) XOR s) AND IR(14));
+    -- slt <= s;
 
 end architecture;
