8fdba74979e63890b7f9fbd5cee5a388f3ac73c0
Aurelien Vilminot <vilminoa@ensipc457.ensimag.fr>
2021-04-14 15:09:24 +0200

    Test sw validé

--- program/autotest/sw.s
+++ program/autotest/sw.s
@@ -1,16 +1,21 @@
 # TAG = sw
-    .data
-    p:.word 50
-
 	.text
-    lui x31, 0
-    lui x30, 0
-    addi x30, x30, 0x003
+    lui x31, 0 # 0x1000
+    lui x30, 1 # 0x1004
+    addi x30, x30, 0x020 # 0x1008
+    
+    addi x29, x29, 0x7DB # 0x100c
+    addi x29, x29, 0x7DB # 0x1010
+    addi x29, x29, 0x001 # 0x1014
+    sw x29, (x30) # # 0x1018
+    lui x31,2 # 0x101C
+    lui x31,5 # 0x1020
+    lui x31,3 # 0x1024
 
-    sw x30, p, x1
-    lw x31, p
 	# max_cycle 100
 	# pout_start
     # 00000000
-	# 00000003
+    # 00002000
+    # 00000000
+    # 00003000
 	# pout_end
