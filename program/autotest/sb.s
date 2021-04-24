8b2865e95f7b5be00a848d0ab3ed749b3612f03e
Aurelien Vilminot <vilminoa@ensipc204.ensimag.fr>
2021-04-24 09:52:20 +0200

    Nouveaux tests SB et SH

--- program/autotest/sb.s
+++ program/autotest/sb.s
@@ -1,25 +1,16 @@
 # TAG = sb
 	.text
-    lui x31, 0 # 0x1000
-    lui x30, 1 # 0x1004
-    addi x30, x30, 0x020 # 0x1008
+    lui x31, 0
+    lui x30, 1
+    addi x30, x30, 0x008
     
-    addi x29, x29, 0x0B7 # 0x100c
+    add x29, x0, 0x1E8
 
-    lui x1, 15 # 0x1010
-    add x31, x0, x1 # 0x1014
-
-    sb x29, (x30) # 0x1018
-    lui x31,2 # 0x101C
-    lui x31,5 # 0x1020
-    add x31, x0, x1 # 0x1024
-    lui x31,3 # 0x1028
+    sb x29, (x30)
+    lw x31, (x30)
 
 	# max_cycle 100
 	# pout_start
     # 00000000
-    # 0000F000
-    # 00002000
-    # 00000000
-    # 00003000
+    # 000000E8
 	# pout_end
--- program/autotest/sh.s
+++ program/autotest/sh.s
@@ -1,23 +1,16 @@
 # TAG = sh
 	.text
-    lui x31, 0 # 0x1000
-    lui x30, 1 # 0x1004
-    addi x30, x30, 0x024 # 0x1008
+    lui x31, 0
+    lui x30, 1
+    addi x30, x30, 0x008
     
-    lui x29, 14 # 0x100c
-    addi x29, x29, 0x7DB # 0x1010
-    addi x29, x29, 0x7DB # 0x1014
-    addi x29, x29, 0x001 # 0x1018
+    lui x29, 0x1E8
 
-    sh x29, (x30) # 0x101C
-    lui x31,2 # 0x1020
-    lui x31,5 # 0x1024
-    lui x31,3 # 0x1028
+    sb x29, (x30)
+    lw x31, (x30)
 
 	# max_cycle 100
 	# pout_start
     # 00000000
-    # 00002000
-    # 0000E000
-    # 00003000
+    # 00008000
 	# pout_end
