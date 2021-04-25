890aaed96d7392d804ab7e66e47e4aaabdae9c85
Aurelien Vilminot <vilminoa@ensipc204.ensimag.fr>
2021-04-25 10:49:35 +0200

    Test SB

--- program/autotest/sb.s
+++ program/autotest/sb.s
@@ -1,16 +1,18 @@
 # TAG = sb
 	.text
     lui x31, 0
-    lui x30, 1
-    addi x30, x30, 0x008
-    
-    add x29, x0, 0x1E8
+    lui x29, 1
 
-    sb x29, (x30)
-    lw x31, (x30)
+    lw x30, storage
+
+    sb x30, 50(x29)
+    lw x31, 50(x29)
+
+storage:
+    .word 1891285
 
 	# max_cycle 100
 	# pout_start
     # 00000000
-    # 000000E8
+    # 000000D5
 	# pout_end
